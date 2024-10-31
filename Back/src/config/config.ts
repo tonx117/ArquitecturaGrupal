7b92e5a8b140d2527871ba00ab68358c4b4783e2
IvanPz01 <ivanciito_999@hotmail.com>
2024-10-31 15:08:21 -0300

    Se creo el dockerfile del front y el docker compose para tanto para el front, back y db

--- Back/src/config/config.ts
+++ Back/src/config/config.ts
@@ -9,7 +9,6 @@ interface Config {
   password: string;
   host?: string;
   port: number; // Cambia de Number a number
->>>>>>> c434ae5fd687fe24026feedd0ab0a24c99736bbd
   dialect: "postgres" | "mysql" | "sqlite" | "mariadb";
 }
 
@@ -29,18 +28,10 @@ for (const varName of requiredEnvVars) {
 }
 
 export const config: Config = {
-<<<<<<< HEAD
-  database: process.env.DB_NAME || "Idiomas",
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "milanesa",
-  host: process.env.DB_HOST || "localhost",
-  port: 5432,
-=======
   database: process.env.DB_NAME || "idiomas",
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "milanesa",
   host: "db" ,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432, // El puerto debe coincidir con el mapeo
->>>>>>> c434ae5fd687fe24026feedd0ab0a24c99736bbd
   dialect: "postgres", 
 };
